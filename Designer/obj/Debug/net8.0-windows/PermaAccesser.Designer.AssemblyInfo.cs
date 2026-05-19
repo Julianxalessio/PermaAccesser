c5c42e04f60193e65483e23c0696d3e6556bdb52
Julianxalessio <julian.lombardo@outlook.com>
2026-05-19 21:56:21 +0200

    screenshot fixed

--- Designer/obj/Debug/net8.0-windows/PermaAccesser.Designer.AssemblyInfo.cs
+++ Designer/obj/Debug/net8.0-windows/PermaAccesser.Designer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermaAccesser.Designer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614c1d7070a997821fc8305f629a52a3658cce36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800ee8bc87abb7196d2aef1890ae19a9e7fe0ae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermaAccesser.Designer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermaAccesser.Designer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
