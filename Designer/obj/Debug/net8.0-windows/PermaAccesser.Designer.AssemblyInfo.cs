a0615386c83a7618d6e2b069556bb58ed7d8ebc5
Julianxalessio <julian.lombardo@outlook.com>
2026-07-02 18:18:04 +0200

    changes iun virus

--- Designer/obj/Debug/net8.0-windows/PermaAccesser.Designer.AssemblyInfo.cs
+++ Designer/obj/Debug/net8.0-windows/PermaAccesser.Designer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermaAccesser.Designer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c42e04f60193e65483e23c0696d3e6556bdb52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2db15e823958b0aecacd2293a0e95d553238332")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermaAccesser.Designer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermaAccesser.Designer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Viruses/RDP.md
@@ -0,0 +1,5 @@
+
+
+
+
+scp "C:\Users\DEIN_LOKALER_NAME\Downloads\uvnc.exe" julia@REMOTE_IP:C:/Users/julia/uvnc.exe
